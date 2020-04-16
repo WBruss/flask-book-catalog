5577edabed4f6f104479b21e4c95b0884b4cc81b
Paul Wainaina <paul.wainaina@tracom.co.ke>
2020-04-16 11:48:16 +0300

    Udemy Flask Course

---
+++ app/auth/forms.py
@@ -0,0 +1,28 @@
+from   flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.auth.models import User
+
+
+def email_exists(form, field):
+    email = User.query.filter_by(user_email=field.data).first()
+    if email:
+        raise ValidationError("Email Already Exists")
+
+
+class RegistrationForm(FlaskForm):
+
+    name = StringField('What is your name', validators=[DataRequired(),
+                                                        Length(3,15, message='between 3 to 15 characters')])
+    email = StringField('Whats is your email', validators=[DataRequired(), Email(), email_exists])
+    password = PasswordField('Password', validators=[DataRequired(), Length(5),
+                                                     EqualTo('confirm', message='Passwords don\'t march')])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
+    register = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    stay_loggedin = BooleanField('stay logged-in')
+    submit = SubmitField('Login')
---
+++ config/prod.py
@@ -0,0 +1,4 @@
+DEBUG = False
+SECRET_KEY='BabaMboga2019'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:Wainaina4384@localhost:6000/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
